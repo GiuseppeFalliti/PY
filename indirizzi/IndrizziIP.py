1181a4a02ba6515b36a7071a0458b359fa6683dc
GiuseppeFalliti <141495925+GiuseppeFalliti@users.noreply.github.com>
2023-10-24 10:13:35 +0200

    Update IndrizziIP.py

--- indirizzi/IndrizziIP.py
+++ indirizzi/IndrizziIP.py
@@ -1,21 +1,25 @@
-
 def get_ip_class(ip_address):
     first_octet = int(ip_address.split('.')[0])
 
     if 1 <= first_octet <= 126:
-        return 'Classe A'
+        ip_class = 'Classe A'
     elif 128 <= first_octet <= 191:
-        return 'Classe B'
+        ip_class = 'Classe B'
     elif 192 <= first_octet <= 223:
-        return 'Classe C'
+        ip_class = 'Classe C'
     elif 224 <= first_octet <= 239:
-        return 'Classe D (Multicast)'
+        ip_class = 'Classe D (Multicast)'
     elif 240 <= first_octet <= 255:
-        return 'Classe E (Riservato)'
+        ip_class = 'Classe E (Riservato)'
     else:
-        return 'Indirizzo IP non valido'
+        ip_class = 'Indirizzo IP non valido'
 
-print("inserisci l'indizzo ip")
-ip_address =input() 
-ip_class = get_ip_class(ip_address)
+    binary_first_octet = bin(first_octet).lstrip('0b').zfill(8) 
+    
+    return ip_class, binary_first_octet
+
+print("Inserisci l'indirizzo IP:")
+ip_address = input()
+ip_class, binary_first_octet = get_ip_class(ip_address)
 print(f"L'indirizzo IP {ip_address} appartiene alla {ip_class}")
+print(f"Primo ottetto in binario: {binary_first_octet}")
